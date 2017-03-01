920d9803a6aa6ce01ed3285ff56af41316cfd6b6
Erin <eringlabe@gmail.com>
2017-03-01 14:26:13 -0800

    code help from class work

--- javascript/app.js
+++ javascript/app.js
@@ -7,8 +7,12 @@ $(document).ready(function() {
 
 	var apiKey = "b9f91d369ff59547cd47b931d8cbc56b:0:74623931";
 
+	var urlBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" +
+  	  apiKey + "&q=";
 
-	function getResults (url) {
+  	var articleCounter = 0;
+
+	function getResults (numArticles, url) {
 
 		$.ajax({
 	    url: url,
@@ -17,17 +21,20 @@ $(document).ready(function() {
 
 	    	var results = response.data;
 	    	
-	    	for (var i = 0; i < results.length; i++) {
-	    		results[i]
+	    	for (var i = 0; i < numArticles; i++) {
+
+	    		articleCounter++;
+
 	    		var link = results[i].docs.web_url;
 	    		var headline = results[i].docs.headline.main;
 	    		var snippet = results[i].docs.snippet;
 	    		var date = results[i].docs.pub_date;
 
 	    		var story = $("<div>").addClass("story");
+	    		story.attr("id", "article-num-" + articleCounter);
+	    		$("#article-display").append(story);
 
 	    		story.append(link).append(headline).append(snippet).append(date);
-	    		$("#results-display").append(story);
 
 	    	}
 
@@ -38,33 +45,44 @@ $(document).ready(function() {
 
 	$("#searchBtn").on("click", function(event){
 		
+		event.preventDefault();
+
+		articleCounter = 0;
+
+		$("#article-display").empty();
+
 		searchTerm = $("#searchTerm").val().trim();
+		var nytURL = urlBase + searchTerm;
+
 		number = $("#number").val().trim();
 		startYear = $("#startYear").val().trim();
 		endYear = $("#endYear").val().trim();
 
-		var nytURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json" + "?=" + apiKey 
-			+ "&q=" + searchTerm;
-		console.log("term " + nytURL);
 
-		if (number > 0) {
-			nytURL = nytURL + "&qf=" + number;
+		if (parseInt(startYear)) {
+		  nytURL = nytURL + "&begin_date=" + startYear + "0101";
 		}
-		console.log("num " + nytURL);
 
-		if (startYear > 0) {
-			nytURL = nytURL + "&begin_date=" + startYear + "0101";
+		if (parseInt(endYear)) {
+    	  nytURL = nytURL + "&end_date=" + endYear + "1231";
 		}
-		console.log("strt " + nytURL);
 
-		if (endYear > 0) {
-			nytURL = nytURL + "&end_date=" + endYear + "1231";
-		}
-		console.log("end " + nytURL);
+		// if (number > 0) {
+		// 	nytURL = nytURL + "&qf=" + number;
+		// }
+		// console.log("num " + nytURL);
 
-		return nytURL;
+		// if (startYear > 0) {
+		// 	nytURL = nytURL + "&begin_date=" + startYear + "0101";
+		// }
+		// console.log("strt " + nytURL);
 
-		getResults(nytURL);
+		// if (endYear > 0) {
+		// 	nytURL = nytURL + "&end_date=" + endYear + "1231";
+		// }
+		// console.log("end " + nytURL);
+
+		getResults(number, nytURL);
 
 	});
 
